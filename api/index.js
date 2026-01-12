7d6357a47ed84eda7169e4108edc038a8467adeb
JacobLef <jacoblefk@gmail.com>
2026-01-12 15:45:24 -0500

    Add fetcher and complete the post request api example.

---
+++ api/index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require("express")
+const app = express()
+const users = []
+const port = process.env.APP_PORT;
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+});
+
+app.get('/', (req, res) => {
+  res.send("Hello users: :) " + users)
+});
+
+// ========== Post ==========
+app.post('/', (req, res) => {
+  if (!req.body || !req.body.name) {
+    res.status(400).send("Missing a name in the request body!");
+    return;
+  }
+  users.push(req.body.name);
+  res.send(`User with name ${req.body.name} has been created`);
+});
+
+// ========= Write your own post request here to do whatever you want ========
